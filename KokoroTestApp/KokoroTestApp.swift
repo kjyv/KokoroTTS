a883de321a5e1074e3286995dd53c69846c93a65
Lassi Maksimainen <lassi.maksimainen@gmail.com>
2026-01-10 23:05:48 +0100

    Updated Test App to use Kokoro 1.0.11 and MLX 0.30.2

--- KokoroTestApp/KokoroTestApp.swift
+++ KokoroTestApp/KokoroTestApp.swift
@@ -11,9 +11,9 @@ struct KokoroTestApp: App {
   /// Initializes the application and configures MLX GPU settings.
   init() {
     // Configure MLX GPU cache limit (50 MB)
-    MLX.GPU.set(cacheLimit: 50 * 1024 * 1024)
+    Memory.cacheLimit = 50 * 1024 * 1024    
     // Configure MLX GPU memory limit (900 MB)
-    MLX.GPU.set(memoryLimit: 900 * 1024 * 1024)
+    Memory.memoryLimit = 900 * 1024 * 1024
   }
 
   var body: some Scene {
